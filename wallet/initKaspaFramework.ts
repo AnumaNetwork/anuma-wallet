f794375599088dca838c9054663456c6ef36ea4f
AnumaDev <anumanetwork@gmail.com>
2024-07-25 19:53:29 +0200

    Delete initKaspaFramework.ts

--- wallet/initKaspaFramework.ts
+++
@@ -1,8 +0,0 @@
-import { Wallet } from './wallet';
-
-export const initAnumaFramework = async () => {
-  // console.log("Anuma - framework: init");
-  await Wallet.initRuntime();
-  // console.log("Anuma - framework: ready");
-};
-
